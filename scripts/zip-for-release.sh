f321f956f734383a64a85cb8beafb81beff2e2d5
Anthony Correa <a@correa.co>
2024-06-28 13:30:25 -0500

    echo version and download in zip script

--- scripts/zip-for-release.sh
+++ scripts/zip-for-release.sh
@@ -1,2 +1,4 @@
+echo "version:"$(cat src/module.json|jq .version)
+echo "download:"$(cat src/module.json|jq .download)
 git archive --format zip --output dist/module.zip master:src
 cp src/module.json dist/
